144d5aa09986dd7513b8542f41ab191b9ae16dff
AndBondStyle <andbondstyle@gmail.com>
2017-08-02 23:02:46 +0300

    More examples

--- web/parsers/sites/__init__.py
+++ web/parsers/sites/__init__.py
@@ -16,22 +16,28 @@ def wrap(func, name):
         objects = [Flat(**i) for i in raw_data]
         # Разом запихиваем все объекты в БД (спасибо bulk_create)
         Flat.objects.bulk_create(objects)
+
     deco.__name__ = 'parse_' + name
-    
+
     return deco
-    
+
 # Подготавливаем таски, заодно создавая несуществующие конфиги
 from pkgutil import iter_modules
 from celery import shared_task
+from annoying.functions import get_object_or_None
+from ..models import *
 
 # Это потом будем импортировать из других модулей
 tasks = {}
 # Идем по модулям в пакете
 for _, name, _ in iter_modules(__path__):
     # Аналогично from . import <name>
-    module = getattr(__import__('sites.' + name), name) 
+    module = getattr(__import__(__package__, fromlist=[name]), name)
     # Проверяем, есть ли там функция parse
     if not hasattr(module, 'parse'): continue
-    # Проверяем наличие конфига
-    
-    tasks[name] = wrap(module.parse, name)
+    # Проверяем наличие конфига и создаем таск
+    task_name = 'parsers.' + name
+    tasks[task_name] = shared_task(name=task_name)(wrap(module.parse, name))
+    if get_object_or_None(Parser, name=name) is None:
+        print('Created parser object with name {}'.format(name))
+        Parser.objects.create(name=name)
