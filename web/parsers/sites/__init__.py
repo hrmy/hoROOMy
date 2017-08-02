4d178282f8d5d7bb637f5f6e81320e7caa17d665
AndBondStyle <andbondstyle@gmail.com>
2017-08-02 22:18:53 +0300

    Sites package task manager update

--- web/parsers/sites/__init__.py
+++ web/parsers/sites/__init__.py
@@ -1,28 +1,37 @@
-﻿# Подготавливаем все, что будет нужно парсерам
+﻿# Подготавливаем здесь все, что будет нужно парсерам...
 import requests
 
-# Прописываем в __all__
-__all__ = ['requests']
 
-# А вот здесь заведем обертку на парсеры
-from functools import wraps
+__all__ = ['requests']  # ... и прописываем в __all__
 
-# Сама обертка
+# Обертка на парсеры
 def wrap(func, name):
-    @wraps(func)
-    def deco(**kwargs):
-        config = ParserConfig.objects.get(name=name)
-        raw_data = func(**config.to_dict())
+    # Конкретно *эта* функция будет вызываться в качестве Celery-таска
+    def deco():
+        # Получаем конфиг для парсера
+        config = Parser.objects.get(name=name).get_config()
+        # Запускаем саму функцию, она должна вернуть итератор по словарям
+        raw_data = func(**config)
+        # Создаем объекты БД из полученных словарей (пока не пушим)
         objects = [Flat(**i) for i in raw_data]
+        # Разом запихиваем все объекты в БД (спасибо bulk_create)
         Flat.objects.bulk_create(objects)
-
+    deco.__name__ = 'parse_' + name
+    
     return deco
     
-# Ну и это для меня
+# Подготавливаем таски, заодно создавая несуществующие конфиги
 from pkgutil import iter_modules
+from celery import shared_task
 
-sites = {}
-for _, name, __ in iter_modules(__path__):
-    module = getattr(__import__('sites.' + name), name)  # Аналогично from . import <name>
+# Это потом будем импортировать из других модулей
+tasks = {}
+# Идем по модулям в пакете
+for _, name, _ in iter_modules(__path__):
+    # Аналогично from . import <name>
+    module = getattr(__import__('sites.' + name), name) 
+    # Проверяем, есть ли там функция parse
     if not hasattr(module, 'parse'): continue
-    sites[name] = wrap(module.parse, name)
+    # Проверяем наличие конфига
+    
+    tasks[name] = wrap(module.parse, name)
