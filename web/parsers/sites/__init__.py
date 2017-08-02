7a310a3694857a37f70686d81db4a69d5f7f0637
AndBondStyle <andbondstyle@gmail.com>
2017-08-02 21:47:32 +0300

    Wrapper update

--- web/parsers/sites/__init__.py
+++ web/parsers/sites/__init__.py
@@ -6,15 +6,15 @@ __all__ = ['requests']
 
 # А вот здесь заведем обертку на парсеры
 from functools import wraps
-# Здесь будет наша моделька (эта для примера)
-from django.contrib.auth.models import User
 
 # Сама обертка
-def wrap(func):
+def wrap(func, name):
     @wraps(func)
     def deco(**kwargs):
-        raw_data = func(**kwargs)
-        for i in raw_data: User.objects.create(**i)
+        config = ParserConfig.objects.get(name=name)
+        raw_data = func(**config.to_dict())
+        objects = [Flat(**i) for i in raw_data]
+        Flat.objects.bulk_create(objects)
 
     return deco
     
@@ -23,5 +23,6 @@ from pkgutil import iter_modules
 
 sites = {}
 for _, name, __ in iter_modules(__path__):
-    parse_func = getattr(__import__('sites.' + name), name).parse
-    sites[name] = wrap(parse_func)
+    module = getattr(__import__('sites.' + name), name)  # Аналогично from . import <name>
+    if not hasattr(module, 'parse'): continue
+    sites[name] = wrap(module.parse, name)
