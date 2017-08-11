87ac0006b422f18e3813c3cad3fb1cdb222bb542
AndBondStyle <andbondstyle@gmail.com>
2017-08-11 13:35:14 +0300

    Clear duplicates - minimal (#3)

--- web/parsers/tasks.py
+++ web/parsers/tasks.py
@@ -1,2 +1,23 @@
 from .sites import tasks as parser_tasks
 from .sites.randomproxy.proxyparser import parse_proxy
+from celery import shared_task
+from .sites.utils import Logger, TgBot
+from .models import *
+
+
+@shared_task(name='parsers.clear_duplicates')
+def clear_duplicates():
+    logger = Logger()
+    logger.name = 'Clear'
+    logger.timestamp('clear')
+    logger.info('Clearing duplicates...')
+    count = 0
+    for ad in Ad.objects.all():
+        if not ad.description: continue
+        qs = Ad.objects.filter(description=ad.description).exclude(id=ad.id)
+        count += len(qs)
+        qs.delete()
+    delta = logger.timestamp('clear')
+    logger.info('Succeed in {:.3f} seconds'.format(delta.seconds))
+    logger.info('Total duplicates removed: {}'.format(count))
+    if count: TgBot.send(logger.get_text())
