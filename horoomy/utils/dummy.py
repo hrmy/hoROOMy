940f1c6f09eeb58c9e236086542bd1c4a4ed84dc
Alexander <akurmazov@mail.ru>
2017-08-24 14:59:17 +0500

    Fixed previous commit

--- horoomy/utils/dummy.py
+++ horoomy/utils/dummy.py
@@ -1,14 +1,14 @@
 
 class Dummy():
 
-	def __call__(self, method):
+	def __call__(self, *args, **kwargs):
 		return self
 
-	def __getattr__(self, name):
+	def __getattr__(self, *args, **kwargs):
 		return self
 
-	def __getitem__(self, index):
+	def __getitem__(self, *args, **kwargs):
 		return self
 
-	def __setitem__(self, index, value):
+	def __setitem__(self, *args, **kwargs):
 		return self
